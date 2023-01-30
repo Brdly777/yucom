c4e3799ce4b77c31b07817c443b14586926f975a
Brdly777 <cristianbe2705@gmail.com>
2023-01-29 23:20:37 -0600

    Se agrego buscador

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yucom/models/presenters_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,170 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SearchPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SearchPage> createState() => _SearchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SearchPageState extends State<SearchPage> {
+  static List<PresentersModel> main_presenters_list = [
+    PresentersModel(
+        "Taco de Ojo",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://yt3.ggpht.com/ytc/AMLnZu9mPR2BTikLV7V9T9l6yST1Gy4-mXPdWtZOY7Ew=s900-c-k-c0x00ffffff-no-rj"),
+    PresentersModel(
+        "Bruja Cuchi Cuchi",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://i0.wp.com/www.hazruido.mx/wp-content/uploads/2019/08/bruja-3.jpg?fit=1200%2C978&ssl=1"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+    PresentersModel(
+        "Cuxum",
+        "Lorem a ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "https://topyucatan.com/storage/uploads/2-1637777286.jpeg"),
+  ];
 
-  void _incrementCounter() {
+  // Listas y filtros que se mostraran
+  List<PresentersModel> display_list = List.from(main_presenters_list);
+
+  void updateList(String value) {
+    // Funcion para el filtrado de la lista.
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      display_list = main_presenters_list
+          .where((element) => element.presenters_name!
+              .toLowerCase()
+              .contains(value.toLowerCase()))
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color(0xffffffff),
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Color.fromARGB(255, 255, 0, 0),
+        elevation: 0.0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "BUSQUEDA",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                onChanged: (value) => updateList(value),
+                style: TextStyle(color: Colors.black54),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.black12,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(27.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: "Ingresa tu busqueda",
+                  prefixIcon: Icon(Icons.mic),
+                  suffixIcon: Icon(Icons.search),
+                ),
+              ),
+              Text(
+                "RESULTADOS",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    height: 2.0),
+              ),
+              SizedBox(
+                height: 17.0,
+              ),
+              // Funcion en caso de no encontrar coincidencias
+              Expanded(
+                child: display_list.length == 0
+                    ? Center(
+                        child: Text(
+                          "Sin coincidencias",
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    : ListView.builder(
+                        itemCount: display_list.length,
+                        itemBuilder: (context, index) => ListTile(
+                          contentPadding: EdgeInsets.all(7.0),
+                          title: Text(
+                            display_list[index].presenters_name!,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            '${display_list[index].presenters_info!}',
+                            style: TextStyle(color: Colors.black54),
+                          ),
+                          leading: Image.network(
+                            display_list[index].presenters_poster_url!,
+                            height: 77,
+                            width: 77,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+              ),
+            ]),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/models/presenters_model.dart
@@ -0,0 +1,8 @@
+class PresentersModel {
+  String? presenters_name;
+  String? presenters_info;
+  String? presenters_poster_url;
+
+  PresentersModel(
+      this.presenters_name, this.presenters_info, this.presenters_poster_url);
+}
