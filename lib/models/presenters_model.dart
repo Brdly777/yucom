45a55279d2615afcabfa96497602f6a53a43e805
Cristian Bradley Be Guerrero <106564277+Brdly777@users.noreply.github.com>
2023-02-27 17:30:46 -0600

    Delete lib directory

--- lib/models/presenters_model.dart
+++
@@ -1,8 +0,0 @@
-class PresentersModel {
-  String? presenters_name;
-  String? presenters_info;
-  String? presenters_poster_url;
-
-  PresentersModel(
-      this.presenters_name, this.presenters_info, this.presenters_poster_url);
-}
